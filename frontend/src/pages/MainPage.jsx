b05cec055b1b7acaccc1ca95307472afa617d394
PolinaShchepochkina <polina007@inbox.ru>
2023-06-06 20:21:29 +0300

    feat(router): add routes

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,5 @@
+function MainPage() {
+  return <h1>Hello!</h1>;
+}
+
+export default MainPage;
---
+++ frontend/src/pages/NumbersPage.jsx
@@ -0,0 +1,5 @@
+function NumbersPage() {
+  return <h1>Hello 2!</h1>;
+}
+
+export default NumbersPage;
