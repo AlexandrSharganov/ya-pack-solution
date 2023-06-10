d93098dea0bc3265706e37c3d2485b44ed73642e
Aleksey Abamzarov <alekseydevweb@gmail.com>
2023-06-10 17:55:03 +0300

    fix: fonts, header, footer, keyboard

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Footer.module.css';
-import FooterButton from '../FooterButton/FooterButton';
+import EnterKeyboardButton from '../EnterKeyboardButton/EnterKeyboardButton';
 
 function Footer({ onProductEntry }) {
+  const location = useLocation();
+  const isProblemPage = location.pathname === '/problem';
+
   return (
-    <section className={styles.footer}>
-      <Link to="/problem" className={styles.link}>
-        <button type="button" className={styles.problemButton}>
-          Есть проблема
-        </button>
-      </Link>
-      {/* <Link to="/keyboard" className={styles.link}> */}
-      <FooterButton onProductEntry={onProductEntry} />
-      {/* </Link> */}
+    <section
+      className={`${styles.footer} ${isProblemPage && styles.footerYellow}`}
+    >
+      {!isProblemPage ? (
+        <>
+          <Link to="/problem" className={styles.link}>
+            <button type="button" className={styles.problemButton}>
+              Есть проблема
+            </button>
+          </Link>
+          <EnterKeyboardButton onProductEntry={onProductEntry} />
+        </>
+      ) : (
+        <Link to="/" className={styles.link}>
+          <button type="button" className={styles.problemButton}>
+            Назад
+          </button>
+        </Link>
+      )}
     </section>
   );
 }
