4da1a4b1acb64bdf05d6e4edf61a59a431a39874
Aleksey Abamzarov <alekseydevweb@gmail.com>
2023-06-16 02:48:48 +0300

    fix: enter package barcode

--- frontend/src/components/AfterScanning/AfterScanning.jsx
+++ frontend/src/components/AfterScanning/AfterScanning.jsx
@@ -1,127 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import styles from './AfterScanning.module.css';
-// import ScanImage from '../../images/scan.svg';
-// import ProductCard from '../ProductCard/ProductCard';
-// import BigButton from '../BigButton/BigButton';
-
-// function AfterScanning({ order, scanProduct, scanNotRecommendedPackage }) {
-//   const [matchingProducts, setMatchingProducts] = useState([]);
-//   const [matchingPackage, setMatchingPackage] = useState(new Set());
-//   const [notMatchingPackages, setNotMatchingPackages] = useState(new Set());
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     if (
-//       (order.skus && scanProduct) ||
-//       (order.skus && scanNotRecommendedPackage)
-//     ) {
-//       const filteredProducts = order.skus.filter(
-//         (item) => item.barcode === scanProduct
-//       );
-//       setMatchingProducts((prevMatchingProducts) => [
-//         ...prevMatchingProducts,
-//         ...filteredProducts,
-//       ]);
-
-//       if (scanProduct && scanProduct.length === 3 && !Number(scanProduct)) {
-//         setMatchingPackage((prevMatchingPackage) =>
-//           prevMatchingPackage.add(scanProduct)
-//         );
-//       }
-
-//       if (scanNotRecommendedPackage) {
-//         setNotMatchingPackages((prevNotMatchingPackages) =>
-//           prevNotMatchingPackages.add(scanNotRecommendedPackage)
-//         );
-//       }
-//     }
-//   }, [order.skus, scanProduct, scanNotRecommendedPackage]);
-
-//   const handleButtonClick = () => {
-//     const isValid = true;
-//     if (isValid) {
-//       navigate('/finish');
-//     }
-//   };
-
-//   const removeProduct = (barcode) => {
-//     setMatchingProducts((prevMatchingProducts) =>
-//       prevMatchingProducts.filter((item) => item.barcode !== barcode)
-//     );
-//   };
-
-//   const renderScanInstructions = () => (
-//     <figure className={styles.figure}>
-//       <img src={ScanImage} alt="Сканер" />
-//       <figcaption>Сканируйте товары из ячейки</figcaption>
-//     </figure>
-//   );
-
-//   const renderProductCards = () => (
-//     <div className={styles.cardList}>
-//       {matchingProducts.map((item) => (
-//         <ProductCard
-//           key={item.barcode}
-//           item={item}
-//           removeProduct={removeProduct}
-//         />
-//       ))}
-//     </div>
-//   );
-
-//   return (
-//     <section className={styles.section}>
-//       <h1 className={styles.title}>Посылка</h1>
-//       <div className={styles.text}>Рекомендованный вид упаковки</div>
-//       <div className={styles.packages}>
-//         {order.packages &&
-//           order.packages.map((item) => (
-//             <div key={item.package} className={styles.package}>
-//               {item.package}
-//             </div>
-//           ))}
-//       </div>
-//       {(matchingPackage.size !== 0 || notMatchingPackages.size !== 0) && (
-//         <>
-//           <div className={styles.text}>Выбранный вид упаковки</div>
-//           <div className={styles.packages}>
-//             {[...matchingPackage].map((item) => (
-//               <div
-//                 key={item}
-//                 className={`${styles.package} ${styles.packageRecommended}`}
-//               >
-//                 {item}
-//               </div>
-//             ))}
-//             {[...notMatchingPackages].map((item) => (
-//               <div
-//                 key={item}
-//                 className={`${styles.package} ${styles.packageNotRecommended}`}
-//               >
-//                 {item}
-//               </div>
-//             ))}
-//           </div>
-//         </>
-//       )}
-//       {!scanProduct && renderScanInstructions()}
-//       {scanProduct && renderProductCards()}
-//       {order.skus &&
-//         (matchingPackage.size !== 0 || notMatchingPackages.size !== 0) &&
-//         matchingProducts.length === order.skus.length && (
-//           <BigButton
-//             isValid
-//             buttonText="Закрыть посылку"
-//             onClick={handleButtonClick}
-//           />
-//         )}
-//     </section>
-//   );
-// }
-
-// export default AfterScanning;
-
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './AfterScanning.module.css';
@@ -138,33 +14,32 @@ function AfterScanning({
   const [matchingProducts, setMatchingProducts] = useState([]);
   const [matchingPackage, setMatchingPackage] = useState(new Set());
   const [notMatchingPackages, setNotMatchingPackages] = useState(new Set());
+  const uniqueBarcodes = new Set();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (
-      (order.skus && scanProduct) ||
-      (order.skus && scanNotRecommendedPackage)
-    ) {
-      const filteredProducts = order.skus.filter(
-        (item) => item.barcode === scanProduct
-      );
-      setMatchingProducts((prevMatchingProducts) => [
-        ...prevMatchingProducts,
-        ...filteredProducts,
-      ]);
-
+    if (order.skus) {
       if (scanProduct) {
+        const filteredProducts = order.skus.filter(
+          (item) => item.barcode === scanProduct
+        );
+        setMatchingProducts((prevMatchingProducts) => [
+          ...prevMatchingProducts,
+          ...filteredProducts,
+        ]);
+      }
+
+      if (scanRecommendedPackage.packagetype) {
         setMatchingPackage((prevMatchingPackage) =>
-          prevMatchingPackage.add(scanRecommendedPackage)
+          prevMatchingPackage.add(scanRecommendedPackage.packagetype)
         );
       }
-      if (scanNotRecommendedPackage) {
-        console.log(scanNotRecommendedPackage);
+
+      if (scanNotRecommendedPackage.packagetype) {
         setNotMatchingPackages((prevNotMatchingPackages) =>
           prevNotMatchingPackages.add(scanNotRecommendedPackage.packagetype)
         );
       }
-      console.log(matchingPackage);
     }
   }, [
     order.skus,
@@ -195,13 +70,21 @@ function AfterScanning({
 
   const renderProductCards = () => (
     <div className={styles.cardList}>
-      {matchingProducts.map((item) => (
-        <ProductCard
-          key={item.barcode}
-          item={item}
-          removeProduct={removeProduct}
-        />
-      ))}
+      {matchingProducts
+        .filter((item) => {
+          if (uniqueBarcodes.has(item.barcode)) {
+            return false;
+          }
+          uniqueBarcodes.add(item.barcode);
+          return true;
+        })
+        .map((item) => (
+          <ProductCard
+            key={item.barcode}
+            item={item}
+            removeProduct={removeProduct}
+          />
+        ))}
     </div>
   );
 
