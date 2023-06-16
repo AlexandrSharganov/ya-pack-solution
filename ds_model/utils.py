50aae9cd0b86456b591a7f000d636be36be9c367
Anton <anton@mcbook.lan>
2023-06-16 15:26:06 +0400

    utils fixed

--- ds_model/utils.py
+++ ds_model/utils.py
@@ -69,38 +69,3 @@ def get_cheapest(s:set) -> str:
         prices = pickle.load(handler)
 
     return sorted(s, key=lambda x: prices.get(x, 1e10))[0]
-
-"""
-def get_packed(jsn: dict, count:int = 1):
-    """Get a packages where goods will fit in
-    works for 1-2-3 items in one order"""
-    if count > 3:
-        return None
-    dt = json.load(open(f'{os.getcwd()}/ds_model/models/borders_data.json', 'r'))['data'][count]
-    rec_pack = []
-    sorted_abc_temp = []
-    # собираем все товары в виде списка [a, b, c]
-    for sku in range(len(jsn['items'])):
-        sorted_abc_temp.append(sorted([jsn['items'][sku]['a'],
-                                       jsn['items'][sku]['b'],
-                                       jsn['items'][sku]['c']]))
-        # минимальный размер умножаем на количество (складываем друг на друга)
-        sorted_abc_temp[sku][0] *= jsn['items'][sku]['count']
-    sorted_abc = sorted(sorted_abc_temp[0])
-    # складываем разные товары по минимальной размерности
-    # из остальных размерностей выбираем максимум (как палки)
-    for i in range(1, len(sorted_abc_temp)):
-        sorted_abc[0] += sorted(sorted_abc_temp[i])[0]
-        sorted_abc[1] = max(sorted_abc_temp[i][1], sorted_abc[1])
-        sorted_abc[2] = max(sorted_abc_temp[i][2], sorted_abc[2])
-    # объем
-    total_volume = sorted_abc[0] * sorted_abc[1] * sorted_abc[2]
-    # подбор
-    for pack in dt:
-        if ((dt[pack]['borders'][0][0] <= sorted_abc[0] <= dt[pack]['borders'][0][1])
-             and (dt[pack]['borders'][1][0] <= sorted_abc[1] <= dt[pack]['borders'][1][1])
-             and (dt[pack]['borders'][2][0] <= sorted_abc[2] <= dt[pack]['borders'][2][1])
-             and (dt[pack]['vol_borders'][0] <= total_volume <= dt[pack]['vol_borders'][1])):
-            rec_pack.append(pack)
-    return tuple(rec_pack)
-"""
