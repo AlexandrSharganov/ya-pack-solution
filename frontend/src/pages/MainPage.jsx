b98bb49deff596b07d17a73e3fb00e4a8c44b389
PolinaShchepochkina <polina007@inbox.ru>
2023-06-18 10:34:04 +0300

    chore: empty

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,35 +1,3 @@
-// import styles from './Pages.module.css';
-// import BeforeScanning from '../components/BeforeScanning/BeforeScanning';
-// import AfterScanning from '../components/AfterScanning/AfterScanning';
-
-// function MainPage({
-//   scanRecommendedPackage,
-//   scanNotRecommendedPackage,
-//   scanProduct,
-//   order,
-//   removeElement,
-//   setRemoveElement,
-// }) {
-//   return (
-//     <section className={styles.main}>
-//       <BeforeScanning
-//         order={order}
-//         scanProduct={scanProduct}
-//         removeElement={removeElement}
-//       />
-//       <AfterScanning
-//         order={order}
-//         scanProduct={scanProduct}
-//         scanRecommendedPackage={scanRecommendedPackage}
-//         scanNotRecommendedPackage={scanNotRecommendedPackage}
-//         removeElement={removeElement}
-//         setRemoveElement={setRemoveElement}
-//       />
-//     </section>
-//   );
-// }
-
-// export default MainPage;
 import React from 'react';
 import styles from './Pages.module.css';
 import BeforeScanning from '../components/BeforeScanning/BeforeScanning';
